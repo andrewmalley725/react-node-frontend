7f159cb42c2dce4aebe2a3074b8f48e226a36098
andrewmalley725 <andrew.alley99@gmail.com>
2022-10-25 15:30:51 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+
+function Main() {
+  const url = "http://localhost:3001/";
+  const [data, setData] = useState({message:''});
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [display, setVool] = useState(false);
+
+  useEffect(() => {
+    axios.get(`${url}api/?name=${name}&email=${email}`).then(result => {
+      setData(result.data)
+    });
+  },[name, email]);
+
+  console.log(data);
+  console.log(name);
+  console.log(email);
+
+  return(
+    <div>
+      Enter Name: <input type='text' onKeyUp={e => setName(e.target.value)}/><br/>
+      Enter Email: <input type='text' onKeyUp={e => setEmail(e.target.value)}/><br/>
+      <button onClick={() => setVool(true)}>Submit</button>
+      <p style={{display : display ? 'block' : 'none'}}>Server says: {data['message']}</p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Main/>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
